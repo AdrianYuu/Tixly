d7378c6618088cd18b0786374f77fa8f2c7d9e32
abedsully <abedsully2909@gmail.com>
2024-10-19 23:02:51 +0700

    feat: added register router & component

--- src/configs/Route.tsx
+++ src/configs/Route.tsx
@@ -2,9 +2,11 @@ import { IDefaultRouter } from "../interfaces/IDefaultRouter";
 
 // Area Import Component Router
 import routerLogin from '../views/Login/Login.router';
+import routerRegister from '../views/Register/Register.router';
 
 export const routes: IDefaultRouter[] = [
-    ...routerLogin
+    ...routerLogin,
+    ...routerRegister
 ]
 
 export default routes;
---
+++ src/views/Register/Register.router.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IDefaultRouter } from '../../interfaces/IDefaultRouter';
+
+const routes: IDefaultRouter[] = [
+  {
+    path: '/register',
+    name: 'Register',
+    component: React.lazy(() => import('./Register')),
+    exact: true,
+  },
+];
+export default routes;
